0a2d86711b9cd11d846ac7a606d9abd50afa4db1
ekberural <ekberural@hotmail.com>
2026-08-18 02:27:16 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const CACHE='budget-v2';
+const CACHE='budget-v3';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
